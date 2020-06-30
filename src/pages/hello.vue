a6f023a85873afcfcc50be570080576f2066c58c
chong peng <178096413@qq.com>
2020-06-30 18:23:28 +0800

    add count

--- src/pages/hello.vue
+++ src/pages/hello.vue
@@ -1,15 +1,21 @@
 <template>
   <h2>
     {{text}}
+    <h3 @click="add">点击增加计数</h3>
+    {{count}}
   </h2>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 @Component({})
 export default class App extends Vue {
-  text: string = 'hello ivy-h5-cli'
+  text: string = 'hello'
+  count: number = 0
   async created() {
     console.log('hello')
+  }
+  add() {
+    this.count++
   }
 }
 </script>
